520532bb8c15c66be81c5ee322a642d19c8f172e
andrewlfauth <andrewlfauth@gmail.com>
2022-09-10 14:10:47 -0700

    Show password toggle

--- app/routes/get-started.jsx
+++ app/routes/get-started.jsx
@@ -1,42 +1,76 @@
 import {Form, useActionData} from '@remix-run/react'
 import {createUser, getUserSession} from '~/services/users.server'
+import {useState} from 'react'
+import {AiOutlineEye, AiOutlineEyeInvisible} from 'react-icons/ai'
 
 export async function action({request}) {
   return createUser(request)
 }
 
-// export async function loader({request}) {
-//   return getUserSession(request)
-// }
+export async function loader({request}) {
+  return getUserSession(request)
+}
 
 function Index() {
   const action = useActionData()
+  const [showPw, setShowPw] = useState(false)
+  const [showPwChanger, setShowPwChanger] = useState(false)
 
   return (
-    <div className="py-32 bg-blue-100">
-      <div className='px-4 sm:px-6 lg:px-8 mx-auto max-w-7xl'>
-        <Form method="post" className="p-10 relative bg-white rounded-md w-[350px]">
+    <div className="flex items-center justify-center h-screen py-32 bg-neutral-50">
+      <div className='px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl'>
+        <Form method="post" className="p-10 shadow border relative bg-white rounded-md w-[350px]">
           {
             action?.error && 
             <span className='absolute bottom-[10px] left-0 right-0 text-center font-semibold text-red-500'>{action.error}</span>
           }
-          <h1 className="font-semibold text-xl mb-12 text-center">Create an Account</h1>
+          <h1 className="mb-12 text-xl font-semibold text-center">Create an Account</h1>
 
           <div className="relative">
             <label htmlFor="username" className="absolute -top-8">
               Create your <span className="font-medium text-blue-900">username</span>
             </label>
-            <input required type="text" name="username" className="bg-blue-50 rounded-md p-1 w-full border outline-blue-900" />
+            <input required type="text" name="username" className="w-full p-1 border rounded-md bg-blue-50 outline-blue-900" />
           </div>
           <div className="relative mt-10">
-            <label htmlFor="password" className="absolute -top-8">
-              Create your <span className="font-medium text-blue-900">password</span>
+            <label htmlFor="password" className="absolute flex items-center -top-8">
+              Create your
+              <span className="ml-1 font-medium text-blue-900">
+                {" "} password
+              </span>
+              {showPwChanger ? (
+                <div>
+                  {showPw ? (
+                      <AiOutlineEyeInvisible 
+                        className='ml-2 text-2xl cursor-pointer text-emerald-500'
+                        onClick={() => setShowPw(false)}
+                      />
+                    ) : (
+                      <AiOutlineEye 
+                        className='ml-2 text-2xl cursor-pointer text-emerald-500'
+                        onClick={() => setShowPw(true)}
+                      />
+                  )}
+                </div>
+              ) : ""}
             </label>
-            <input required type="text" name="password" className="bg-blue-50 rounded-md p-1 w-full border outline-blue-900" />
+            <input 
+              required 
+              type={showPw ? "text" : "password"} 
+              name="password" 
+              className="w-full p-1 border rounded-md bg-blue-50 outline-blue-900" 
+              onChange={(e) => {
+                if (e.target.value.length) {
+                  setShowPwChanger(true)
+                } else {
+                  setShowPwChanger(false)
+                }
+              }}
+            />
           </div>
           <button
             type="submit"
-            className="rounded-md w-full py-2 bg-purple-400 mt-6 font-semibold"
+            className="w-full py-2 mt-6 font-semibold text-white bg-blue-900 rounded-md select-none"
           >
             Sign-up
           </button>
