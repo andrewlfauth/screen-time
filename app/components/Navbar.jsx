2ea8e4f5e82ee903f4a77646409f701aeddf3701
andrewlfauth <andrewlfauth@gmail.com>
2022-09-10 12:26:37 -0700

    styles

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,7 +2,7 @@ import {FaRegLightbulb} from 'react-icons/fa'
 
 function Navbar({toggle}) {
   return (
-    <div className='z-50 absolute border left-0 top-0 flex justify-between w-[95vw]'>
+    <div className='absolute top-0 left-0 z-50 flex justify-between w-screen py-2'>
       <div>
         <span className='hidden lg:block'>large</span>
         <span className='hidden md:block lg:hidden'>medium</span>
