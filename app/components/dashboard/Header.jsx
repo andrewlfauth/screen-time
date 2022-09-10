f719ffecde6423e36d32511412221f4e0b3cf3dc
andrewlfauth <andrewlfauth@gmail.com>
2022-09-10 13:27:41 -0700

    Splice params array based on length

--- app/components/dashboard/Header.jsx
+++ app/components/dashboard/Header.jsx
@@ -1,14 +1,15 @@
 import {useLocation, Link} from '@remix-run/react'
 
 function Header() {
-  const pathArr = useLocation().pathname.split('/').filter(p => p)
+  let pathArr = useLocation().pathname.split('/').filter(p => p)
+  if (pathArr.length === 4) pathArr = pathArr.splice(3,1)
 
   return (
     <header className='pb-6 mr-20'>
-        <h1 className="text-center flex justify-center items-center select-none text-lg md:text-xl lg:text-2xl flex-wrap font-semibold">
+        <h1 className="flex flex-wrap items-center justify-center text-lg font-semibold text-center select-none md:text-xl lg:text-2xl">
           {pathArr.map((s,i) => (
             i === pathArr.length - 1 ? 
-              <span key={i} className='text-emerald-700 text-xl md:text-2xl lg:text-3xl whitespace-nowrap'>{s.replaceAll('%20', " ")}</span> :
+              <span key={i} className='text-xl text-emerald-700 md:text-2xl lg:text-3xl whitespace-nowrap'>{s.replaceAll('%20', " ")}</span> :
               <div key={i} className="flex">
                 <Link
                   to={i === 0 ? '/dashboard' : `/dashboard/${s}`} 
