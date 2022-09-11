456eef5ec12f725310b906296f1fc6bf49fc284e
andrewlfauth <andrewlfauth@gmail.com>
2022-09-10 21:49:08 -0700

    Show password functionality

--- app/routes/login.jsx
+++ app/routes/login.jsx
@@ -1,5 +1,7 @@
 import {loginUser} from '../services/users.server'
 import {Form, useActionData} from '@remix-run/react'
+import {useState} from 'react'
+import {AiOutlineEyeInvisible, AiOutlineEye} from 'react-icons/ai'
 
 export async function action({request}) {
   return loginUser(request)
@@ -7,6 +9,8 @@ export async function action({request}) {
 
 function Index() {
   const action = useActionData()
+  const [showPw, setShowPw] = useState(false)
+  const [showPwChanger, setShowPwChanger] = useState(false)
 
   return (
     <div className="py-32 bg-blue-100">
@@ -25,16 +29,47 @@ function Index() {
             <input required type="text" name="username" className="bg-blue-50 rounded-md p-1 w-full border outline-blue-900" />
           </div>
           <div className="relative mt-10">
-            <label htmlFor="password" className="absolute -top-8">
-              Enter your <span className="font-medium text-blue-900">password</span>
+          <label htmlFor="password" className="absolute flex items-center -top-8">
+              Create your
+              <span className="ml-1 font-medium text-blue-900">
+                {" "} password
+              </span>
+              {showPwChanger ? (
+                <div>
+                  {showPw ? (
+                      <AiOutlineEyeInvisible 
+                        className='ml-2 text-2xl cursor-pointer text-emerald-500'
+                        onClick={() => setShowPw(false)}
+                      />
+                    ) : (
+                      <AiOutlineEye 
+                        className='ml-2 text-2xl cursor-pointer text-emerald-500'
+                        onClick={() => setShowPw(true)}
+                      />
+                  )}
+                </div>
+              ) : ""}
             </label>
-            <input required type="text" name="password" className="bg-blue-50 rounded-md p-1 w-full border outline-blue-900" />
+            
+            <input 
+              required 
+              type={showPw ? "text" : "password"} 
+              name="password" 
+              className="bg-blue-50 rounded-md p-1 w-full border outline-blue-900"
+              onChange={(e) => {
+                if (e.target.value.length) {
+                  setShowPwChanger(true)
+                } else {
+                  setShowPwChanger(false)
+                }
+              }} 
+            />
           </div>
           <button
             type="submit"
-            className="rounded-md w-full py-2 bg-purple-400 mt-6 font-semibold"
+            className="rounded-md w-full text-white py-2 bg-blue-900 mt-6 font-semibold"
           >
-            Sign-up
+            Login
           </button>
         </Form>
       </div>
