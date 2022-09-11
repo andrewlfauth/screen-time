bea2ce9f69557c90895f084a29462750fb6e9a65
andrewlfauth <andrewlfauth@gmail.com>
2022-09-10 22:26:56 -0700

    /dashboard index route

---
+++ app/routes/dashboard/index.jsx
@@ -0,0 +1,11 @@
+import FeaturedShow from '~/components/dashboard/FeaturedShow'
+
+function Index() {
+  return (
+    <div className='px-2 py-1'>
+      <FeaturedShow />
+    </div>
+  )
+}
+
+export default Index
