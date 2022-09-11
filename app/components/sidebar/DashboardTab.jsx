713a71c2c27cd1773557dcfedb29b208eff69f2f
andrewlfauth <andrewlfauth@gmail.com>
2022-09-10 23:34:18 -0700

    Dashboard tab

---
+++ app/components/sidebar/DashboardTab.jsx
@@ -0,0 +1,21 @@
+import {Link} from '@remix-run/react'
+import {BsUiRadiosGrid} from 'react-icons/bs'
+
+function DashboardTab({expand}) {
+  
+  return (
+    <div className='select-none'>
+      <Link 
+        to='/dashboard'
+        className={`${expand ? "justify-between" : "justify-center"} flex lg:justify-between font-semibold text-2xl tracking-tight items-center shadow bg-white p-4 rounded-md group`}
+      >
+        <span className={`${expand ? "block" : "hidden lg:block"}`}>Dash</span>
+        <BsUiRadiosGrid
+        className='text-[30px] text-blue-900 group-hover:scale-110'
+        />
+      </Link>
+    </div>
+  )
+}
+
+export default DashboardTab
--- app/components/sidebar/Sidebar.jsx
+++ app/components/sidebar/Sidebar.jsx
@@ -2,6 +2,7 @@ import ShowsTab from './ShowsTab'
 import FavoritesTab from './FavoritesTab'
 import LogoutTab from './LogoutTab'
 import PlansTab from './PlansTab'
+import DashboardTab from './DashboardTab'
 import {useEffect, useState} from 'react'
 import {AiOutlineDoubleRight} from 'react-icons/ai'
 import {useTransition} from '@remix-run/react'
@@ -17,6 +18,7 @@ function Sidebar() {
   return (
     <div className={`${expand ? "z-10 w-[200px] px-2" : "px-1 lg:px-2"} bg-gray-100 pb-2 pt-[88px] rounded-md fixed z-10 top-0 lg:w-[205px] duration-300 h-screen flex flex-col justify-between`}>
       <div className='space-y-2'>
+        <DashboardTab expand={expand} />
         <PlansTab expand={expand} />
         <ShowsTab expand={expand} />
         <FavoritesTab expand={expand} />
