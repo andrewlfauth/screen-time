7f5d19d40b3f4cd0a46de7d6b94f3082aeb5a784
andrewlfauth <andrewlfauth@gmail.com>
2022-09-15 16:14:21 -0700

    featured plan model

---
+++ app/models/FeaturedPlans.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+
+const FeaturedPlansSchema = mongoose.Schema({
+  plans: Array,
+  expireAt: {
+    type: Date,
+    expires: '720m', default: Date.now()
+  } 
+}, 
+)
+
+const FeaturedPlans = mongoose.models.FeaturedPlans || mongoose.model("FeaturedPlans", FeaturedPlansSchema)
+
+export default FeaturedPlans
