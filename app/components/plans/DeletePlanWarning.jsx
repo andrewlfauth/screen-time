a77135d1669b8d08996b289b11a894b8db4a7d69
andrewlfauth <andrewlfauth@gmail.com>
2022-09-14 22:16:51 -0700

    Disable button and loading indicator on submission

--- app/components/plans/DeletePlanWarning.jsx
+++ app/components/plans/DeletePlanWarning.jsx
@@ -1,17 +1,24 @@
-import { Form } from "@remix-run/react"
+import { Form, useTransition } from "@remix-run/react"
 
 function DeletePlanWarning({planName, show, cancel}) {
+  const transition = useTransition()
+
   return (
     <div className='absolute z-50 p-4 bg-white rounded shadow right-4 top-4'>
       <p className='text-center'>
         Are you sure you want to delete
         <br /> 
-        <span className='text-lg font-medium text-blue-900'>{planName}</span>
+        {transition.submission ? (
+          <span className='text-lg font-medium text-red-500'>Deleting...</span>
+        ) : (
+          <span className='text-lg font-medium text-blue-900'>{planName}</span>
+        )}
       </p>
         <div className="flex justify-center mt-4 space-x-4">
           <Form method="post">
             <button 
               type="submit"
+              disabled={transition.submission}
               className="px-4 py-2 font-semibold text-white duration-100 bg-blue-900 rounded hover:bg-opacity-90"
             >
               Yes 👍
