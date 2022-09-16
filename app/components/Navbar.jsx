4f61d7602462b95a38eef5080ff5550bdb933334
andrewlfauth <andrewlfauth@gmail.com>
2022-09-15 22:18:22 -0700

    remove dark mode

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,19 +1,10 @@
-import {FaRegLightbulb} from 'react-icons/fa'
-
-function Navbar({toggle}) {
+function Navbar() {
   return (
-    <div className='absolute top-0 left-0 z-50 flex justify-between w-full px-4 py-2'>
-      <div>
-        <span className='hidden lg:block'>large</span>
-        <span className='hidden md:block lg:hidden'>medium</span>
-        <span className='hidden sm:block md:hidden'>small</span>
-        <span className='sm:hidden'>tiny</span>
-      </div>
-      <button 
-          onClick={toggle}
-        >
-          <FaRegLightbulb className="text-2xl" />
-        </button>
+    <div className='fixed top-0 left-0 z-50 flex justify-between w-full lg:px-4 py-2 px-3'>
+      {/* <div className="flex items-end">
+        <img src="https://res.cloudinary.com/dpnkrz8c8/image/upload/w_30/v1663304991/Screen%20Time/simple-classic-telivision_1308-111236_a38ita.jpg" alt="" className="w-6 lg:w-auto" />
+        <span className="font-bold hidden lg:block">Screen Time</span>
+      </div> */}
     </div>
   )
 }
