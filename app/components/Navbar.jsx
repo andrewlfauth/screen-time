df8fde3ea414f8b8a4b660a23564d22c5925d6d2
andrewlfauth <andrewlfauth@gmail.com>
2022-09-16 14:35:13 -0700

    removed tailwind screen size spans

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import {BsInfoCircle} from 'react-icons/bs'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from '@remix-run/react'
+
 
 function Navbar() {
+  const path = useLocation().pathname
+
   return (
     <div className='absolute top-0 left-0 z-50 flex justify-end w-full px-3 py-2 lg:px-4'>
       <Link to="/info" title="Info">
-        <BsInfoCircle className='text-lg text-gray-400 hover:text-gray-600 lg:text-2xl' />
+        <BsInfoCircle className={`${path === '/info' ? "text-gray-600" : "text-gray-400 hover:text-gray-600"} text-lg lg:text-2xl`} />
       </Link>
     </div>
   )
