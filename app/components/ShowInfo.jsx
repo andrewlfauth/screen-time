b168d55d263ec0fb5ca45b46b3522c0f15e303d4
andrewlfauth <andrewlfauth@gmail.com>
2022-09-16 16:22:33 -0700

    styling

--- app/components/ShowInfo.jsx
+++ app/components/ShowInfo.jsx
@@ -5,24 +5,24 @@ function ShowInfo({show, toggle}) {
   const [showDescription, setShowDescription] = useState(false)
 
   return toggle ? (
-    <div className="h-full select-none realtive w-full text-white bg-blue-900 bg-opacity-90 py-2 absolute rounded-t-md overflow-y-auto">
+    <div className="absolute w-full h-full py-2 overflow-y-auto text-white bg-blue-900 border-t-8 border-blue-900 select-none border-opacity-10 realtive bg-opacity-90 rounded-t-md">
       <div 
-        className='p-1 cursor-pointer z-10 absolute top-1 left-1'
+        className='absolute z-10 p-1 cursor-pointer top-1 left-1'
         onClick={() => setShowDescription(!showDescription)}
       >
         <RiArrowUpDownFill className='text-2xl text-slate-300'/>
       </div>
-      <span className='text-center font-medium text-2xl block'>
+      <span className='block text-2xl font-medium text-center'>
         {showDescription ? "Description" : "Learning Goals"}
       </span>
-      <div className='p-2 bg-slate-300 font-medium mx-4 text-blue-900 rounded-md  mt-2'>
+      <div className='p-2 mx-4 mt-2 font-medium text-blue-900 rounded-md bg-slate-300'>
         {showDescription ? (
           <p>{show.description}</p>
         ) : (
           <div>
             {show.focus.map((f,i) => 
               <h2 
-                className='whitespace-nowrap mr-2' 
+                className='mr-2 whitespace-nowrap' 
                 key={f}
               >
                   {f}
