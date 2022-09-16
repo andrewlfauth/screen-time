bcad10a9e99328a0bbbcda6914b13a3f7f89c177
andrewlfauth <andrewlfauth@gmail.com>
2022-09-16 13:57:55 -0700

    new image

--- app/components/plans/Hero.jsx
+++ app/components/plans/Hero.jsx
@@ -12,11 +12,11 @@ function Hero({action, currentPlan, clearPlan, removeImage, focusOptions, onChan
   
   return (
     <div className='flex justify-between w-full px-4 py-6 bg-white rounded-md shadow sm:pt-8 sm:pb-4'>
-      <div className='max-w-sm relative'>
-        <h2 className='text-2xl font-semibold tracking-tighter sm:text-4xl mb-2'>
+      <div className='relative max-w-sm'>
+        <h2 className='mb-2 text-2xl font-semibold tracking-tighter sm:text-4xl'>
         Start a new plan
         </h2>
-        <p className='max-w-xs items-center mb-6 text-sm tracking-tight text-gray-600'>
+        <p className='items-center max-w-xs mb-6 text-sm tracking-tight text-gray-600'>
           <span>
             Choose the topics that you want your child to learn. Click
           </span>
@@ -64,8 +64,8 @@ function Hero({action, currentPlan, clearPlan, removeImage, focusOptions, onChan
           />
         </div>
       </div>
-        <div className='select-none hidden mt-4 sm:block'>
-          <img src="https://res.cloudinary.com/dpnkrz8c8/image/upload/w_200/v1663024120/Screen%20Time/image_49_imj1iq.png" alt="fox" />
+        <div className='hidden mt-10 select-none sm:block'>
+          <img src="https://res.cloudinary.com/dpnkrz8c8/image/upload/w_180/v1663361694/Screen%20Time/card_7_ohj31q.png" alt="fox" />
         </div>
     </div>
   )
