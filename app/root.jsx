6bd1fd692b600f9264309ff25a9523f177d5bc55
andrewlfauth <andrewlfauth@gmail.com>
2022-09-16 14:26:54 -0700

    Error and Catch boundaries

--- app/root.jsx
+++ app/root.jsx
@@ -3,6 +3,8 @@ import {connect} from './services/db.server'
 import Navbar from './components/Navbar'
 import { useLocation } from '@remix-run/react';
 import Nav from './components/dashboard/Nav'
+import NotFound from './components/NotFound'
+import ErrorPage from './components/NotFound'
 
 const {
   Links,
@@ -24,6 +26,38 @@ export const links = () => {
     {rel: "stylesheet", href: tailwindURL},
     {rel: "icon", href: "https://res.cloudinary.com/dpnkrz8c8/image/upload/w_20/v1663012532/Screen%20Time/image_13_1_nycrjx.png"},
   ]
+}
+
+export const CatchBoundary = () => {
+  return (
+    <html>
+      <head>
+        <title>Oops!</title>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <NotFound />
+        <Scripts />
+      </body>
+    </html>
+  )
+}
+
+export const ErrorBoundary = () => {
+  return (
+    <html>
+      <head>
+        <title>Oops!</title>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <ErrorPage />
+        <Scripts />
+      </body>
+    </html>
+  )
 }
 
 export async function loader() {
