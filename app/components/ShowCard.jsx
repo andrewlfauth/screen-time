8357b0d3e8023f49a16e80bc4d5b5e3fae08e5cd
andrewlfauth <andrewlfauth@gmail.com>
2022-09-16 14:48:22 -0700

    change icon to close when info is showing

--- app/components/ShowCard.jsx
+++ app/components/ShowCard.jsx
@@ -4,6 +4,7 @@ import LikeButton from './LikeButton'
 import AddButton from './AddButton'
 import ShowInfo from './ShowInfo'
 import {BiInfoCircle} from 'react-icons/bi'
+import {AiOutlineCloseCircle} from 'react-icons/ai'
 
 
 function ShowCard({show, action = "like", onClick, added}) {
@@ -16,22 +17,26 @@ function ShowCard({show, action = "like", onClick, added}) {
         <img src={show.image} alt={show.title} className='w-full aspect-auto rounded-t-md' />
       </div>
 
-      <div className="bg-blue-900 flex items-center justify-between rounded-b-md shadow px-2 py-1">
+      <div className="flex items-center justify-between px-2 py-1 bg-blue-900 shadow rounded-b-md">
         <button
           className='relative group' 
           onClick={() => setShowInfo(!showInfo)}
         >
-          <div className="absolute opacity-0 group-hover:opacity-100 group-hover:scale-100 duration-100 delay-200 scale-75 -top-12 bg-white text-center text-black font-bold py-1 w-24 rounded-full -left-2 shadow text-sm pointer-events-none">
+          <div className="absolute w-24 py-1 text-sm font-bold text-center text-black duration-100 delay-200 scale-75 bg-white rounded-full shadow opacity-0 pointer-events-none group-hover:opacity-100 group-hover:scale-100 -top-12 -left-2">
             <div className="w-2 h-2 bg-white absolute left-4 rotate-45 -bottom-[4px]"></div>
               {showInfo ? "Close" : "Info"}
           </div>
-          <BiInfoCircle className="text-white text-2xl" />
+          {showInfo ? 
+            <AiOutlineCloseCircle className="text-2xl text-white" />
+          :
+            <BiInfoCircle className="text-2xl text-white" />
+          }
         </button>
 
         <h3 
-          className='text-white relative flex group items-center cursor-help font-semibold tracking-tight'
+          className='relative flex items-center font-semibold tracking-tight text-white group cursor-help'
         >
-            <div className="absolute shadow scale-75 -top-12 opacity-0 group-hover:opacity-100 group-hover:scale-100 duration-100 delay-200 bg-white text-center text-black font-bold py-1 w-24 rounded-full -left-3 text-sm pointer-events-none">
+            <div className="absolute w-24 py-1 text-sm font-bold text-center text-black duration-100 delay-200 scale-75 bg-white rounded-full shadow opacity-0 pointer-events-none -top-12 group-hover:opacity-100 group-hover:scale-100 -left-3">
               <div className="w-2 h-2 bg-white absolute left-0 right-0 mx-auto rotate-45 -bottom-[4px]"></div>
               Ages
             </div>
