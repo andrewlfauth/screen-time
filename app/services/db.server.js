22764a4340f6ade6b05d8fc2454e097aedfd187b
andrewlfauth <andrewlfauth@gmail.com>
2022-10-10 16:55:32 -0700

    New: database user

--- app/services/db.server.js
+++ app/services/db.server.js
@@ -14,12 +14,9 @@ async function connect() {
   } else {
     if (!global.__db) {
       global.__db = await mongoose
-        .connect(
-          'mongodb+srv://Greybph:Strong-Bow-72@cluster0.lek0yfy.mongodb.net/screen-time?retryWrites=true&w=majority',
-          {
-            useNewUrlParser: true,
-          }
-        )
+        .connect(process.env.MONGODB_URL, {
+          useNewUrlParser: true,
+        })
         .catch((err) => console.error(err))
     }
     db = global.__db
